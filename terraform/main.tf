9c2846a671c2af53926b42a94ea4679a5732a87c
Oleg Skogorev <oskogorev@gmail.com>
2020-04-07 01:51:22 +0400

    Configure main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,20 +13,28 @@ resource "google_compute_instance" "app" {
   name = "reddit-app"
   machine_type = "g1-small"
   zone = "europe-west1-b"
-  tags = ["reddit-app"]
   boot_disk {
     initialize_params {
       image = "reddit-base"
     }
   }
-  network_interface {
-    network = "default"
-    access_config {}
-  }
   metadata = {
     # путь до публичного ключа
     ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
   }
+  tags = ["reddit-app"]
+    network_interface {
+    network = "default"
+    access_config {}
+  }
+  connection {
+    type = "ssh"
+    host = self.network_interface[0].access_config[0].nat_ip
+    user = "appuser"
+    agent = false
+    # путь до приватного ключа
+    private_key = file("~/.ssh/appuser")
+}
   provisioner "file" {
     source = "files/puma.service"
     destination = "/tmp/puma.service"
