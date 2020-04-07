c4edf82e77071aba306c56ac998b372248d10556
Oleg Skogorev <oskogorev@gmail.com>
2020-04-07 18:49:21 +0400

    Add Variables for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
 terraform {
-# Версия terraform
-required_version = "0.12.8"
+  # Версия terraform
+  required_version = "0.12.8"
 }
 provider "google" {
-# Версия провайдера
-version = "2.15"
-# ID проекта
-project = "var.project"
-region = "var.region"
+  # Версия провайдера
+  version = "2.15"
+  # ID проекта
+  project = var.project
+  region  = var.region
 }
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone = "europe-west1-b"
+  zone         = "europe-west1-b"
   boot_disk {
     initialize_params {
       image = var.disk_image
@@ -23,25 +23,25 @@ resource "google_compute_instance" "app" {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
   tags = ["reddit-app"]
-    network_interface {
+  network_interface {
     network = "default"
     access_config {}
   }
   connection {
-    type = "ssh"
-    host = self.network_interface[0].access_config[0].nat_ip
-    user = "appuser"
+    type  = "ssh"
+    host  = self.network_interface[0].access_config[0].nat_ip
+    user  = "appuser"
     agent = false
     # путь до приватного ключа
-    private_key = file("~/.ssh/appuser")
-}
+    private_key = file(var.private_key_path)
+  }
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
-}
+  }
   provisioner "remote-exec" {
     script = "files/deploy.sh"
-}
+  }
 }
 resource "google_compute_firewall" "firewall_puma" {
   name = "allow-puma-default"
@@ -50,10 +50,10 @@ resource "google_compute_firewall" "firewall_puma" {
   # Какой доступ разрешить
   allow {
     protocol = "tcp"
-    ports = ["9292"]
-  }  
-   # Каким адресам разрешаем доступ
-   source_ranges = ["0.0.0.0/0"]
-   # Правило применимо для инстансов с перечисленными тэгами
-   target_tags = ["reddit-app"]
+    ports    = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
 }
