b5a518b45ba68bcfcaf08bb4d43a1d5375ee3d4d
Oleg Skogorev <oskogorev@gmail.com>
2020-04-17 00:55:13 +0400

    storage-bucket

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,14 +1,20 @@
+terraform {
+  required_version = "~> 0.12.0"
+}
+
 provider "google" {
-version = "~> 2.15"
-project = var.project
-region = var.region
+  version = "~> 2.5.0"
+  project = var.project
+  region  = var.region
 }
+
 module "storage-bucket" {
-source = "SweetOps/storage-bucket/google"
-version = "0.1.1"
-# Имена поменяйте на другие
-name = ["storage-bucket-olegsk", "storage-bucket-olegsk2"]
+  source   = "SweetOps/storage-bucket/google"
+  version  = "0.3.1"
+  name     = "storage-bucket-terraform-devops"
+  location = var.region
 }
+
 output storage-bucket_url {
-value = module.storage-bucket.url
+  value = module.storage-bucket.url
 }
